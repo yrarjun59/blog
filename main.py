4e6faeddbe60443f18b4888c9fac333dc3de79e2
Arjun Thapa <yrarjun59@gmail.com>
2022-09-16 08:34:59 +0545

    updated header

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from datetime import date
+import os
 
 from sqlalchemy import ForeignKey
 from werkzeug.exceptions import abort
@@ -16,7 +17,7 @@ from flask_gravatar import Gravatar
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -29,7 +30,7 @@ today = datetime.today()
 date_today = today.strftime("%Y")
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
